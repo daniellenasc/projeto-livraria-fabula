ca005bb87fce39a7702a22a67214e11877245965
Camila Beisiegel <camila.beisiegel@gmail.com>
2022-02-14 15:58:09 -0300

    add books page

---
+++ src/pages/Home/index.js
@@ -0,0 +1,13 @@
+import logo from '../../assets/logo-livrariaFabula.png'
+
+
+export function Home(){
+
+    return (
+        <div className="">
+
+        <img src={logo} alt="logo-"/>
+
+        </div>
+    )
+}
