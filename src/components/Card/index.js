8f4aa9f92a3b76e3f324a542b924e8906992eaea
Camila Beisiegel <camila.beisiegel@gmail.com>
2022-02-14 14:18:53 -0300

    inserindo o componente Card

---
+++ src/components/Card/index.js
@@ -0,0 +1,46 @@
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+
+export function ImgMediaCard() {
+  return (
+    <Card sx={{ maxWidth: 300 }}>
+
+      <CardMedia 
+        component="img"
+        alt="book-cover"
+        item xs={2}
+        image="https://images-na.ssl-images-amazon.com/images/I/41UBaYAAeNS.jpg"
+        //image={props.image}
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+        Dubliners
+          {/* {props.title} */}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+        James Joyce
+          {/* {props.author} */}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+        12.99
+          {/* {props.price} */}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+        Detalhes
+          {/* {props.price} */}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        {/* Aqui vamos ter 3 links, dois buttons e um link para detalhes de livros */}
+        <Button size="small">Detalhes</Button>
+        <Button size="small">Share</Button>
+        <Button size="small">Learn More</Button>
+      </CardActions>
+    </Card>
+  );
+}
