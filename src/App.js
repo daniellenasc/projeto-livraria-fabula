ecfceb55bb0e85cb1c98cd6d518e168e2d505c87
Camila Beisiegel <camila.beisiegel@gmail.com>
2022-02-14 17:28:15 -0300

    ajeitando pull

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-
 import { Navbar } from "./components/Navbar"
 import { Footer } from "./components/Footer"
 import {Books} from "./pages/Books"
