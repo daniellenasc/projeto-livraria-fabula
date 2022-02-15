92a96a180f162c2435abdea0b723991922e57b90
Camila Beisiegel <camila.beisiegel@gmail.com>
2022-02-14 21:23:54 -0300

    fixing problem

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,21 +1,22 @@
-import Style from "./navbar.css"
+
 import { Link } from "react-router-dom"
+import './navbar.css'
 export function Navbar() {
 
   return (
     <nav>
       
       <input type="checkbox" id="check_nav"></input>
-		  <label for="check_nav" class="checkbtn_nav">
-			  <i class="fas fa-bars"></i>
+		  <label htmlFor="check_nav" className="checkbtn_nav">
+			  <i className="fas fa-bars"></i>
 		  </label>
-		  <label class="logo">Livraria Fábula</label>
+		  <label className="logo">Livraria Fábula</label>
 		  <ul>			
 
         <li><Link to="/" className="linkNav"> Home </Link></li>
         <li><Link to="/livros" className="linkNav"> Livros </Link></li>
         <li><Link to="/cadastro" className="linkNav"> Cadastro </Link></li>
-        <li><Link to="/contato" className="linkNav"> Contato </Link></li>
+        <li><Link to="/contato" className="linkNav"> Contato </Link></li> 
 
 		  </ul>
     </nav>
